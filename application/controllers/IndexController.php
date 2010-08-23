166aa0869bbd1dd33480c679412e1d1211c9184e
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2010-08-23 10:33:13 +0200

    Add options from ini

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -2,9 +2,17 @@
 
 class IndexController extends Zend_Controller_Action
 {
+    protected $_config;
+
+    public function preDispatch()  
+    {
+            $this->_config = new Zend_Config_Ini('../application/configs/'.
+                    'application.ini', APPLICATION_ENV);
+    }
+
     public function indexAction()
     {
-         $db = new Phly_Couch(array("db"=>"test-log", "host"=>"localhost", "port"=>"5984"));
+         $db = new Phly_Couch($this->_config->couchdb);
 
          $this->view->form = $form = new App_Form_Filter();
          if ($this->getRequest()->isPost()) {
@@ -12,12 +20,12 @@ class IndexController extends Zend_Controller_Action
              if ($form->isValid($formData)) {
                  $filterValue = $form->getValue('filter');
                  if (empty($filterValue)) $filterValue = null;
-                 $result = $db->view('log_by_prior','log_by_prior', $filterValue, array("db"=>"test-log"));
+                 $result = $db->view('log_by_prior','log_by_prior', $filterValue, array("db"=>$this->_config->couchdb->db));
              } else {
              $form->populate($formData);
              }
          } else {
-            $result = $db->view('log_by_prior','log_by_prior', null, array("db"=>"test-log"));         
+            $result = $db->view('log_by_prior','log_by_prior', null, array("db"=>$this->_config->couchdb->db));         
          }         
          $this->view->docs = $result->toArray();
 
@@ -26,7 +34,7 @@ class IndexController extends Zend_Controller_Action
     public function logAction()
     {
           $logger = new Zend_Log();
-          $logger->addWriter(new App_Log_Writer_CouchDb("test-log"));
+          $logger->addWriter(new App_Log_Writer_CouchDb($this->_config->couchdb->db));
           echo "Logged action";
           $logger->log("Testovani chyba", Zend_Log::ERR);
     }
